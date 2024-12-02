ca0f3732db3b54a47dae9f4cf695d96afbb7ae7e
mahfujalarony <149530147+mahfujalarony@users.noreply.github.com>
2024-12-02 21:54:12 +0600

    something update

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,6 +1,15 @@
 var acc = document.getElementsByClassName("according");
 var i;
 
+if(acc.length > 0){
+    acc[0].classList.add("active");
+    var panel = acc[0].nextElementSibling;
+    if(panel) {
+        panel.style.display = "block";
+        panel.style.backgroundColor = "#b9ff66";
+    }
+}
+
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function(){
         this.classList.toggle = "active";
@@ -10,7 +19,7 @@ for (i = 0; i < acc.length; i++) {
         
         } else {
             panel.style.display = "block";
-            
+            panel.style.backgroundColor = "#b9ff66"
         }
     })
 }
